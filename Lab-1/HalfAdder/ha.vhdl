7d95480df5fe93608659650d5b3937105c0c345d
Ella DiSanti <122648382+edisanti@users.noreply.github.com>
2024-05-01 22:29:54 -0400

    Create ha.vhdl

---
+++ Lab-1/HalfAdder/ha.vhdl
@@ -0,0 +1,18 @@
+library ieee;               
+use ieee.std_logic_1164.all;
+
+entity ha is
+    port
+    (
+        a:  in  std_ulogic; -- Input of the unresolved type
+        b:  in  std_ulogic; -- https://vhdlwhiz.com/std_logic-vs-std_ulogic/
+        s:  out std_ulogic; -- Output sum of a and b
+        c:  out std_ulogic  -- Output carry
+    );
+end ha;
+
+architecture behave of ha is
+begin
+    s <= a xor b;
+    c <= a and b;
+end behave;
